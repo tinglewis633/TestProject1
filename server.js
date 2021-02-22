3d011fd702d0df8f7ae596db2b88d8ec9ad5fefc
Lewis <72407518+tinglewis633@users.noreply.github.com>
2021-02-22 15:24:47 -0500

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const connectDB = require("./config/db");
+
 const express = require("express");
 
 const app = express();
+
+connectDB();
 
 app.get("/", (req, res) => res.send("API RUNNING"));
 const PORT = process.env.PORT || 5000;
