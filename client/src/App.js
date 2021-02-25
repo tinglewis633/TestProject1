768e339cef1a9031cb1b1d3af84dcfee4fd6b997
Lewis <72407518+tinglewis633@users.noreply.github.com>
2021-02-25 16:57:43 -0500

    Install react, landing page

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import Landing from "./components/Landing";
+
+const App = () => (
+  <Fragment>
+    <Landing />
+  </Fragment>
+);
+
+export default App;
